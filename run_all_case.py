cfa467bba3ea5f383246043e112b40631ddc83a1
zxh0803 <963795293@qq.com>
2017-11-13 15:06:35 +0800

    change file path

--- run_all_case.py
+++ run_all_case.py
@@ -5,13 +5,13 @@ from common.HTMLTestRunner import HTMLTestRunner
 from common.send_mail import sendmail
 def all_case():
 
-    case_path = 'D:\\autotest\\case'
+    case_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),"case")
     discover = unittest.defaultTestLoader.discover(case_path,pattern='test*.py',top_level_dir=None)
     return discover
 
 if __name__ == '__main__':
 
-    report_path = os.path.join("D:\\autotest\\report","result.html")
+    report_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),"report/result.html")
     with open(report_path,"wb") as fp:
         runner = HTMLTestRunner(stream=fp,title=u"自动化测试报告：",description=u"执行情况：")
         runner.run(all_case())
